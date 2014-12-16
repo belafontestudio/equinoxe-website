de9ddaa0486d2142a8dee78f1bdd9964e24f9d90
Gabriele Marchi <gabriele@belafonte.co>
2014-12-16 17:20:50 +0100

    latest yacht landing

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
     $('#onepage').fullpage();
-    // Options
-var options = {
-    offset: 1000
-}
-
-// Create a new instance of Headhesive and pass in some options
-//var header = new Headhesive('.subMenu', options);
+    console.log("dfsdgfdfg");
 });
