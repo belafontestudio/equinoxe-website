d38afc7417f6dc9f6b4ee63346ec420cb336a36e
Piermaria Cosina <piermaria@belafonte.co>
2014-12-16 19:00:34 +0100

    small image + some fixes

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
     $('#onepage').fullpage();
-    console.log("dfsdgfdfg");
-});
+    height = $(window).height();
+    $('ul#latest-yachts-imgbackground li img').height(height);
+    });
