b620d580b2e1d484993273fdd18e9fed8c10b493
Piermaria Cosina <piermaria@belafonte.co>
2014-12-17 17:21:49 +0100

    menu plugin with dummy text

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
     $('#onepage').fullpage();
     height = $(window).height();
     $('ul#latest-yachts-imgbackground li img').height(height);
+    $('#simple-menu').sidr();
     });
