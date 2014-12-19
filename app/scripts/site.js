f2a13a76c3c9710efc1a031d550d0582db96acb7
Piermaria Cosina <piermaria@belafonte.co>
2014-12-19 18:24:44 +0100

    fixed yellow menu and external style for menu

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -2,6 +2,30 @@ $(document).ready(function() {
     $('#onepage').fullpage();
     height = $(window).height();
     $('ul#latest-yachts-imgbackground li img').height(height);
-    $('#simple-menu').sidr();
+    $('#simple-menu').sidr({
+    	onOpen : menuOpen,
+    	onClose : menuClose
+	});
 
+
+    function menuOpen() {
+    	var menu = $('a#simple-menu');
+    	menu.addClass('hide');
+		if (menu.hasClass('show')){
+			menu.removeClass('show');
+		}
+    };
+    function menuClose() {
+    	var menu = $('a#simple-menu');
+    	menu.addClass('show');
+    	if (menu.hasClass('hide')){
+    		menu.removeClass('hide');
+    	}
+    };
+
+    $('a#closeMenu').click(function(){
+    	$.sidr("close");
     });
+
+});
+
