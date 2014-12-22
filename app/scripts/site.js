ad40457aa473ed1e7671931f7fc07e4877e65b1a
Piermaria Cosina <piermaria@belafonte.co>
2014-12-22 16:17:04 +0100

    menu with highlight and section recognition
    + sidemenu styling

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
     $('#onepage').fullpage({
+        menu: '#anchor-menu',
+        anchors:['landing','yacht-charter', 'sale-purchase','expeditions','heritage'],
         resize: false,
-        autoScrolling: false,
+        autoScrolling: true,
         slidesNavigation: true,
-        slidesNavPosition: 'bottom'
+        slidesNavPosition: 'bottom',
+        afterLoad: function(anchorLink, index){
+            var hmenu = $('div#landing-menu div.center')
+            //using anchorLink
+            
+            if(anchorLink != 'landing'){
+                if (!hmenu.hasClass('show')){
+                    hmenu.addClass('show');
+                }
+                if (hmenu.hasClass('fast-hide')){
+                    hmenu.removeClass('fast-hide');
+                }
+                
+            }
+        },
+        onLeave: function(index, nextIndex, direction){
+            var menu = $('div#landing-menu div.center')
+            if(index == 2 && direction == 'up'){
+                hmenu.addClass('fast-hide');
+                if (hmenu.hasClass('show')){
+                    hmenu.removeClass('show');
+                }
+                
+            }  
+            
+        }
+
     });
     height = $(window).height();
     var container_width = $('ul#latest-yachts-imgbackground li').width();
@@ -27,6 +55,16 @@ $(document).ready(function() {
 		if (menu.hasClass('show')){
 			menu.removeClass('show');
 		}
+        var hmenu = $('div#landing-menu div.center');
+            
+        if (!hmenu.hasClass('hide')){
+            hmenu.addClass('hide');
+        }
+        if (hmenu.hasClass('show')){
+            hmenu.removeClass('show');
+        }
+          
+        
     };
     function menuClose() {
     	var menu = $('a#simple-menu');
@@ -34,6 +72,14 @@ $(document).ready(function() {
     	if (menu.hasClass('hide')){
     		menu.removeClass('hide');
     	}
+        var hmenu = $('div#landing-menu div.center');
+            
+        if (!hmenu.hasClass('show')){
+            hmenu.addClass('show');
+        }
+        if (hmenu.hasClass('hide')){
+            hmenu.removeClass('hide');
+        }
     };
 
     $('a#closeMenu').click(function(){
